35d5041386f30766d3cdaba5f793051b8a882b11
Flavio Lima <flavio.lima981@gmail.com>
2023-08-07 09:11:48 -0300

    implementa badge de quantidade de itens

--- src/components/Snacks/index.tsx
+++ src/components/Snacks/index.tsx
@@ -14,14 +14,22 @@ interface SnacksProps {
 }
 
 export function Snacks({ snacks }: SnacksProps) {
-  const { addSnackIntoCart } = useCart()
+  const { cart, addSnackIntoCart } = useCart()
 
   return (
     <Container>
       {!snacks.length
         ? [1,2,3,4].map((n) => <SkeletonSnack key={n} />)
-        : snacks.map((snack) => (
+        : snacks.map((snack) => {
+          const snackExistent = cart.find(
+            (item) => item.snack === snack.snack && item.id === snack.id,
+          )
+
+          return (
             <div key={snack.id} className="snack">
+
+              {snackExistent && <span>{snackExistent.quantity}</span>}
+
               <h2>{snack.name}</h2>
               <img src={snack.image} alt={snack.name} />
               <p>{snack.description}</p>
@@ -32,7 +40,8 @@ export function Snacks({ snacks }: SnacksProps) {
                 </button>
               </div>
             </div>
-        ))}
+          )
+        })}
     </Container>
   )
 }
