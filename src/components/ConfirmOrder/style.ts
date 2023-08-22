bbef62d34fbcfbd26ecde1086ee7a6088b96242f
Flavio Lima <flavio.lima981@gmail.com>
2023-08-22 09:32:29 -0300

    corrige espacameto botao fina.Pedido

--- src/components/ConfirmOrder/style.ts
+++ src/components/ConfirmOrder/style.ts
@@ -2,6 +2,8 @@ import { darken } from "polished";
 import styled from "styled-components";
 
 export const Container = styled.footer`
+  padding-top: 1.5rem;
+
   display: flex;
   align-items: center;
   justify-content: space-between;
