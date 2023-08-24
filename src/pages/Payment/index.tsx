f3e977129dff178da9bbf35932bba5953f16a94e
Flavio Lima <flavio.lima981@gmail.com>
2023-08-24 10:16:17 -0300

    cria campos para dados pessoas no form

--- src/pages/Payment/index.tsx
+++ src/pages/Payment/index.tsx
@@ -1,6 +1,6 @@
 import { Head } from "../../components/Head";
 import { OrderHeader } from "../../components/OrderHeader";
-import { Container, Inner } from "./styles";
+import { Container, Inner, Form } from "./styles";
 
 export default function Payment() {
   return (
@@ -8,7 +8,32 @@ export default function Payment() {
       <Head title='Pagamento' />
       <OrderHeader />
       <Inner>
-        form de pagamento
+        <Form>
+          <h4>Informações pessoais</h4>
+
+          <div className="field">
+            <label htmlFor="full_name">Nome e sobrenome</label>
+            <input type="text" name="full_name" id="full_name" autoComplete="name" />
+          </div>
+
+          <div className="grouped">
+            <div className="field">
+              <label htmlFor="email">E-mail</label>
+              <input type="email" name="email" id="email" autoComplete="email" />
+            </div>
+
+            <div className="field">
+              <label htmlFor="mobile">Celular</label>
+              <input type="tel" name="mobile" id="mobile" autoComplete="phone" />
+            </div>
+
+            <div className="field">
+              <label htmlFor="document">CPF / CNPJ</label>
+              <input type="text" name="document" id="document" />
+            </div>
+          </div>
+
+        </Form>
       </Inner>
     </Container>
   )
