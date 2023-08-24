85d04153167d63d17f8450a1a713ff32ec601aaa
Flavio Lima <flavio.lima981@gmail.com>
2023-08-24 09:36:02 -0300

    cria pag de pagamento

--- src/routes.tsx
+++ src/routes.tsx
@@ -6,6 +6,7 @@ import PizzasPage from './pages/Main/Pizzas'
 import DrinksPage from './pages/Main/Drinks'
 import DessertsPage from './pages/Main/Desserts'
 import MyCartPage from './pages/MyCart'
+import Payment from './pages/Payment'
 
 export function AppRoutes() {
   return (
@@ -17,6 +18,7 @@ export function AppRoutes() {
         <Route path='desserts' element={<DessertsPage />} />
       </Route>
       <Route path='cart' element={<MyCartPage />} />
+      <Route path='Payment' element={<Payment />} />
     </Routes>
   )
 }
